23adae91a0ca95f05c064b4f4a018f2aab223142
AlexItskoff <119017869+AlexItskoff@users.noreply.github.com>
2022-12-21 02:27:13 +0300

    Add files via upload

---
+++ Test41.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine("Enter length of array");       // Имитируем ввод пользователем произвольного 
+int arrayLength = int.Parse(Console.ReadLine()!);   // количетва положительных и отрицательных чисел
+
+int[] array = CreateArray(arrayLength, -500, 500);    // Создаем массив
+PrintArray(array);                                    // Выводим массив на печать
+CountPositiveNumbers(array);                          // Считаем количество положительных чисел
+
+int[] CreateArray(int length, int minValue, int maxValue)  // Функция создания массива
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)                                     // Прцедура печати массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void CountPositiveNumbers(int[] arr)                     // Процедура подсчета положительных чисел
+{
+    int count = 0;
+     for (int i = 0; i < arrayLength; i++)
+     {
+        if (arr[i]>0)
+        {
+            count = count +1;
+        }
+    }
+        Console.WriteLine( " amount of positive numbers" +  " - "  + count);
+ } 
+ 
---
+++ Test43.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+// Вводим начальные значения переменных ******************************
+Console.WriteLine("Enter b1");
+double b1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter k1");
+double k1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter b2");
+double b2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter k2");
+double k2 = int.Parse(Console.ReadLine()!);
+// Проверяем, что прямые не параллельны ***********************************
+if (k1 == k2)
+{
+    Console.WriteLine("Lines are parallel! Crossing point does not exist!");
+    return;
+}
+//***********************************************************************************
+double [] CrossingPoint =  CrossingLines(b1, k1, b2, k2);  // Создаем массив, дающий искомые координаты
+Console.WriteLine("x = " + CrossingPoint[0]);             // Выводи конечный  резульат на печать
+Console.WriteLine("y = " +CrossingPoint[1]);
+
+//************************************************************************************
+// Функция, создающая массив из двух вещественных чисел. 
+// Координаты точки пересечения могут быть дробными
+double []  CrossingLines (double b1, double k1, double b2, double k2) 
+  {
+    double[] result = new double[2];
+    double x = (b2 - b1)/ (k1 - k2);
+    double y = (k1 * (b2 - b1) / (k1 - k2)) + b1;
+
+    result [0] = x;
+    result [1] = y;
+    return (result);
+
+    }
+// ***********************************************************************************
